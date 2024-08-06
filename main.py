2a8016abf6deebf892d9ca2113c809bfefcbbbf3
Ebrahim Nezaparast Bonab <jnazamprast2@gmail.com>
2024-08-06 22:07:16 +0330

    i finished

--- main.py
+++ main.py
@@ -1,207 +1,210 @@
-import curses
-import random
-import time
 import os
+import time
+import random
+import curses
 
-def main(stdscr):
-    # Initialize curses
-    curses.curs_set(0)  # Hide the cursor
+# Global variables
+food = []
+enemy = []
+black_hole = []
+player = []
+score = 0
+enemy_move_interval = 0.1
+last_enemy_move_time = time.time()
+num_food_items = random.randint(1, 20)
+num_enemy = random.randint(5, 8)
+num_black_hole = random.randint(1, 2)
+play = True
+current_time = time.time()
+maxl = 0
+maxc = 0
+food_age = 100
+player_char = '🛸'
+enemy_char = '👾'
+food_char = '🍕'
+black_hole_char = '🌀'
+
+def init_curses(stdscr):
+    curses.curs_set(0)
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
+    print("Curses initialized.")
 
-    maxl = curses.LINES - 1
-    maxc = curses.COLS - 1
-    food_age = 100
-    player_char = '🛸'
-    food_char = '🍕'
-    enemy_char = '👾'
-    black_hole_char = '🌀'
+def random_place(world, maxl, maxc):
+    while True:
+        a = random.randint(0, maxl - 1)
+        b = random.randint(0, maxc - 1)
+        if world[a][b] == ' ':
+            return a, b
 
-    player_c = player_l = 0
-    score = 0
-    enemy = []
-    food = []
-    black_hole = []
+def init_world(stdscr):
+    global world, player, food, enemy, black_hole
     world = []
+    for i in range(maxl + 1):
+        world.append([])
+        for j in range(maxc + 1):
+            if random.random() > 0.05:
+                world[i].append(' ')
+            else:
+                world[i].append('.')
+    
+    for _ in range(num_food_items):
+        fl, fc = random_place(world, maxl, maxc)
+        fa = random.randint(food_age, food_age * 10)
+        food.append((fl, fc, fa, food_char))   
 
-    enemy_move_interval = 0.1  # Enemies move every 0.5 seconds
-    last_enemy_move_time = time.time()
+    for _ in range(num_enemy):
+        el, ec = random_place(world, maxl, maxc)
+        enemy.append((el, ec, enemy_char))  
 
-    def random_place():
-        """Generate random coordinates for placing player or food."""
-        while True:
-            a = random.randint(0, maxl - 1)
-            b = random.randint(0, maxc - 1)
-            if world[a][b] == ' ':
-                return a, b
+    for _ in range(num_black_hole):
+        bl, bc = random_place(world, maxl, maxc)
+        black_hole.append((bl, bc, black_hole_char))
+    
+    player_l, player_c = random_place(world, maxl, maxc)
+    player = [player_l, player_c, player_char]
+    print("World initialized.")
 
-    def init_world():
-        """Initialize the world with random dots and spaces."""
-        nonlocal player_c, player_l, world, food, enemy, black_hole
-        world = []
-        for i in range(maxl + 1):
-            world.append([])
-            for j in range(maxc + 1):
-                world[i].append(' ' if random.random() > 0.03 else '.')
+def in_range(a, min_val, max_val):
+    return max(min(a, max_val), min_val)
 
-        food = []
-        for i in range(random.randint(1, 20)):
-            fl, fc = random_place()
-            fh = random.randint(food_age, food_age*10)
-            food.append((fl, fc, fh))
+def draw(stdscr):
+    stdscr.clear()
+    for i in range(maxl):
+        for j in range(maxc):
+            stdscr.addch(i, j, world[i][j])
+    # Show the score of the player
+    stdscr.addstr(1, 1, f"Score: {score}")
 
-        enemy = []
-        for i in range(30):
-            el, ec = random_place()
-            enemy.append((el, ec))
+    # Draw food
+    for f in food:
+        fl, fc, fa, fch = f
+        stdscr.addch(fl, fc, fch)
 
-        black_hole = []
-        for i in range(2):
-            bl, bc = random_place()
-            black_hole.append((bl, bc))
+    # Draw enemies
+    for e in enemy:
+        el, ec, ech = e
+        stdscr.addch(el, ec, ech)
 
-        player_l, player_c = random_place()
-        return world
+    # Draw black holes  
+    if score > 50:
+        for b in black_hole:
+            bl, bc, bch = b
+            stdscr.addch(bl, bc, bch)
 
-    def in_range(a, min_val, max_val):
-        if a > max_val:
-            return max_val
-        if a < min_val:
-            return min_val
-        return a
+    # Draw player  
+    pl, pc, pch = player
+    stdscr.addch(pl, pc, pch)
+    stdscr.refresh()
 
-    def draw(world):
-        """Draw the world grid on the screen."""
-        # Draw world
-        stdscr.clear()
-        for i in range(maxl):
-            for j in range(maxc):
-                stdscr.addch(i, j, world[i][j])
-                
-        # Show score
-        stdscr.addstr(1, 1, f"score: {score}")
-        # Showing the food
-        for f in food:
-            fl, fc, fa = f
-            stdscr.addch(fl, fc, food_char)
-        # Showing enemy
-        for e in enemy:
-            el, ec = e
-            stdscr.addch(el, ec, enemy_char)
-        if score > 50:
-            for b in black_hole:
-                bl, bc = b
-                stdscr.addch(bl, bc, black_hole_char)    
+def move_player(key):
+    if key in ['w', 'W'] and world[player[0] - 1][player[1]] != '.':
+        player[0] -= 1
+    elif key in ['s', 'S'] and world[player[0] + 1][player[1]] != '.':
+        player[0] += 1
+    elif key in ['d', 'D'] and world[player[0]][player[1] + 1] != '.':
+        player[1] += 1
+    elif key in ['a', 'A'] and world[player[0]][player[1] - 1] != '.':
+        player[1] -= 1
 
-        # Showing the player
-        stdscr.addch(player_l, player_c, player_char)
-        stdscr.refresh()
+    player[0] = in_range(player[0], 0, maxl - 1)
+    player[1] = in_range(player[1], 0, maxc - 1)
 
-    # Initialize the world
-    world = init_world()
-    draw(world)
+def check_food():
+    global score
+    for i in range(len(food)):
+        fl, fc, fa, fch = food[i]
+        fa -= 1
+        if player[0] == fl and player[1] == fc:
+            score += 10
+            fl, fc = random_place(world, maxl, maxc)
+            fa = random.randint(food_age, food_age * 10)
+            food[i] = (fl, fc, fa, fch)
+        if fa <= 0:
+            fl, fc = random_place(world, maxl, maxc)
+            fa = random.randint(food_age, food_age * 10)
+        food[i] = (fl, fc, fa, fch)
 
-    def move(key):
-        """Move in one direction based on aswd."""
-        nonlocal player_c, player_l
-
-        if key == 'w' and world[player_l - 1][player_c] != '.':
-            player_l -= 1
-        elif key == 's' and world[player_l + 1][player_c] != '.':
-            player_l += 1
-        elif key == 'd' and world[player_l][player_c + 1] != '.':
-            player_c += 1
-        elif key == 'a' and world[player_l][player_c - 1] != '.':
-            player_c -= 1
-
-        player_l = in_range(player_l, 0, maxl - 1)  # Prevent moving out of bounds
-        player_c = in_range(player_c, 0, maxc - 1)  # Prevent moving out of bounds
-
-    def check_food():
-        nonlocal score
-        for i in range(len(food)):
-            fl, fc, fh = food[i]
-            fh -= 1
-            if player_l == fl and player_c == fc:
-                score += 10
-                fl, fc = random_place()
-                fh = random.randint(food_age, food_age * 10)
-                food[i] = (fl, fc, fh)
-            if fh <= 0:
-                fl, fc = random_place()
-                fh = random.randint(food_age, food_age * 10)
-            food[i] = (fl, fc, fh)
-
-    def check_black_hole():
+def check_black_hole(stdscr):
+    if score > 50:
         for i in range(len(black_hole)):
-            bl, bc = black_hole[i]
-            if player_l == bl and player_c == bc:
-                curses.endwin()  # End curses mode before printing to the terminal or launching another script
+            bl, bc, bch = black_hole[i]
+            if player[0] == bl and player[1] == bc:
+                curses.endwin()
                 stdscr.clear()
                 stdscr.addstr(maxl // 2, maxc // 2, "!!! YOU FOUND A HIDDEN WAY TO INTERSTELLAR JOURNEY !!!")
                 stdscr.refresh()
                 time.sleep(5)
-                os.system('python another_script.py')
-                exit() 
+                exit()
 
-    def move_enemy():
-        nonlocal play
-        for i in range(len(enemy)):
-            l, c = enemy[i]
-            if random.random() > 0.6:
-                if l > player_l:
-                    l -= 2
-                elif l < player_l:
-                    l += 2
-                elif c < player_c:
-                    c += 2
-                elif c > player_c:
-                    c -= 2
+def move_enemy(stdscr):
+    global play
+    for e in range(len(enemy)):
+        el, ec, ech = enemy[e]
+       
+        if random.random() > 0.8 and el > player[0]:
+            el -= 1
+        elif random.random() > 0.8 and  el < player[0]:
+            el += 1
+        if random.random() > 0.8 and ec < player[1]:
+            ec += 1
+        elif random.random() > 0.8 and  ec > player[1]:
+            ec -= 1
 
-                l = in_range(l, 0, maxl - 1)
-                c = in_range(c, 0, maxc - 1)
-                enemy[i] = (l, c)
-            if l == player_l and c == player_c:
-                stdscr.addstr(maxl // 2, maxc // 2, "YOU DIED!!!!")
-                stdscr.refresh()
-                time.sleep(3)
-                play = False
+        el = in_range(el, 0, maxl - 1)
+        ec = in_range(ec, 0, maxc - 1)
 
-    # Wait for user input to exit or refresh
-    play = True
+        enemy[e] = (el, ec, ech)
+
+        if el == player[0] and ec == player[1]:
+            stdscr.addstr(maxl // 2, maxc // 2, "YOU DIED!!!!")
+            stdscr.refresh()
+            time.sleep(3)
+            play = False
+
+def run(stdscr):
+    global play, current_time
     while play:
         current_time = time.time()
         try:
-            key = stdscr.getkey()
+            key = stdscr.getkey()    
         except:
-            key = ''
-
-        if key == 'q':  # Press 'q' to exit
+            key = ' '
+        if key == 'q':
             play = False
-        elif key == 'r':  # Press 'r' to refresh the screen
-            world = init_world()
-            draw(world)
-        elif key in 'asdw':
-            move(key)
+        elif key == 'r':
+            init_world(stdscr)
+            draw(stdscr)               
+        elif key in 'awsdASWD':
+            move_player(key)   
 
+        move_enemy(stdscr)
         check_food()
-        check_black_hole()
-        if current_time - last_enemy_move_time > enemy_move_interval:
-            move_enemy()
-            last_enemy_move_time = current_time
-
+        check_black_hole(stdscr)
+        
+        draw(stdscr)
         time.sleep(0.01)
-        draw(world)
+
+def main(stdscr):
+    global maxl, maxc
+    maxl = curses.LINES - 1
+    maxc = curses.COLS - 1
+
+    print("Initializing curses...")
+    init_curses(stdscr)
+    print("Initializing world...")
+    init_world(stdscr)
+    print("Starting game loop...")
+    run(stdscr)
 
 if __name__ == "__main__":
     try:
+        print("Starting game...")
         curses.wrapper(main)
     except KeyboardInterrupt:
-        # Ensure the terminal is reset on interrupt
         curses.endwin()
         print("Exited program")
     except Exception as e:
-        # Ensure the terminal is reset on any other exception
         curses.endwin()
         print(f"An error occurred: {e}")
